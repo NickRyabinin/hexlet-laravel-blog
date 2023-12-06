57e9a2342d49e9f9d2f57c3140272a65c819ce52
Николай <rnn2002@mail.ru>
2023-12-06 14:38:25 +0300

    Modify Exports/ArticlesExport - change some styling in exported xlsx file

--- app/Exports/ArticlesExport.php
+++ app/Exports/ArticlesExport.php
@@ -6,13 +6,20 @@ use App\Models\Article;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ArticlesExport implements FromCollection, WithHeadings, WithStyles
+class ArticlesExport implements
+    FromCollection,
+    WithHeadings,
+    WithStyles,
+    ShouldAutoSize,
+    WithColumnWidths
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Article::all();
@@ -24,14 +31,26 @@ class ArticlesExport implements FromCollection, WithHeadings, WithStyles
             'ID',
             'Заголовок',
             'Содержание',
-            'Создатель',
+            'ID создателя',
             'Опубликовано',
             'Изменено'
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'C' => 55,
+            'E' => 28,
+            'F' => 28,
         ];
     }
 
     public function styles(Worksheet $sheet)
     {
         $sheet->getStyle(1)->getFont()->setBold(true);
+        $sheet->getStyle('C')->getAlignment()->setWrapText(true);
+        // $sheet->getDefaultRowDimension()->setRowHeight(50);
+        // $sheet->getRowDimension(1)->setRowHeight(-1);
     }
 }
