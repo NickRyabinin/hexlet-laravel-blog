625b43bf8f4a6a6f12dfda98cebeda3d64894fbc
Николай <rnn2002@mail.ru>
2023-12-07 18:12:42 +0300

    Add current temperature in my place from open-meteo API to About page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,6 +9,13 @@ class PageController extends Controller
     public function about()
     {
         $keywords = ['PHP', 'Laravel', 'Blade', 'Eloquent', 'PostgreSQL', 'Breeze', 'Tailwind CSS'];
-        return view('page.about', ['tags' => $keywords]);
+        $url = 'https://api.open-meteo.com/v1/forecast?latitude=54.625&longitude=43.875' .
+        '&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,' .
+        'showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,' .
+        'wind_gusts_10m&daily=sunrise,sunset&timezone=Europe%2FMoscow&forecast_days=1';
+        $contentJson = file_get_contents($url);
+        $contentArray = json_decode($contentJson, true);
+        $currentTemperature = $contentArray['current']['temperature_2m'];
+        return view('page.about', ['tags' => $keywords, 'currentTemperature' => $currentTemperature]);
     }
 }
--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -12,6 +12,9 @@
         Стилизация - Tailwind CSS.
         В продакшене используется БД PostgreSQL, в локальном окружении - SQLite.</p>
     <p class="my-4">Теги: {{ implode(', ', $tags) }}</p>
+    <div class="my-4">
+        Температура сейчас где-то в Мордовии: {{ $currentTemperature }}°C
+    </div>
     <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
     </div>
